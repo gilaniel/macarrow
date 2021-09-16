052f189e6eb380ee998cb48efe341907a6f4c199
gila <gila@134-0-118-193.cloudvps.regruhosting.ru>
2021-09-16 13:07:10 +0300

    node ssl

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const fs = require("fs");
+const https = require("https");
 const axios = require("axios").default
 const bodyParser = require("body-parser")
 
@@ -8,13 +10,20 @@ app.use(bodyParser.json())
 
 var corsOptions = {
   origin: [
-    "https://macarrowstudio.com/",
-    "https://www.macarrowstudio.com/",
+    "https://macarrowstudio.com",
+    "https://www.macarrowstudio.com",
     "http://134.0.118.193",
     "http://134.0.118.193:3000",
   ],
   optionsSuccessStatus: 200,
 }
+
+const keysDir = "/etc/ssl/";
+const options = {
+  key  : fs.readFileSync(keysDir + "macarrowstudio.key"),
+  ca   : fs.readFileSync(keysDir + "certrequest.csr"),
+  cert : fs.readFileSync(keysDir + "macarrowstudio.crt")
+};
 
 const chatId = -598218104
 const botToken = "1979480195:AAHoGddof2q_4pg2jHnFxvWIdNNErmg1X-0"
@@ -48,4 +57,4 @@ app.post("/message", cors(corsOptions), function (req, res) {
     })
 })
 
-app.listen(3000)
+https.createServer(options, app).listen(3000);
